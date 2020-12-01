a8ca387da73e78235ba2b9e050c054771f638bae
SHANMUKH69 <37172194+SHANMUKH69@users.noreply.github.com>
2020-12-01 18:44:11 +0530

    finding two numbers and there product

--- Day1/Day1_q1_sol.py
+++ Day1/Day1_q1_sol.py
@@ -1,3 +1,4 @@
+from termcolor import colored
 #Find the two entries that sum to 2020!
 #what do you get if you multiply them together?
 def _Q1(n):
@@ -7,8 +8,16 @@ def _Q1(n):
             #wulavrus operator: assignment expression
             #to know more about wulavrus operator
             #https://github.com/SHANMUKH-CH/pythonPrograms/blob/main/tricks%20and%20tips/walrus%20operator.py
-            print(f"2 numbers and there product: {A_num}, {_2nd_number} and {A_num * _2nd_number}")
+            return (colored(f'by usual way {A_num * _2nd_number}','blue'))
+#q1 solution 2 using itertools and combinations
+import itertools
+
+def _Q1_iterway(n):
+    lol = [ x * y for x, y in itertools.combinations(map(int, n), 2) if x + y  == 2020]
+    print(colored(f'using itertools {lol[0]}','red'))
+    
 if __name__=='__main__':
     with open('my_input.txt') as f:
         numbers_in_file = list(sorted(map(int,f)))
-        _Q1(numbers_in_file)
+        print(_Q1(numbers_in_file))
+        _Q1_iterway(numbers_in_file)
