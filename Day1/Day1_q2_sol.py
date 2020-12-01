f527b45b73c0d3dbd95d3193dcac8fc1e9af0e65
SHANMUKH69 <37172194+SHANMUKH69@users.noreply.github.com>
2020-12-01 18:24:46 +0530

    solution for 2nd part

---
+++ Day1/Day1_q2_sol.py
@@ -0,0 +1,12 @@
+import itertools
+'''
+https://docs.python.org/3/library/itertools.html#module-itertools
+itertools.combinations(iterable, r): Return r length subsequences of elements from the input iterable.
+'''
+def _Q2(n):
+    lol = [x*y*z for x, y, z in itertools.combinations(map(int, n), 3) if x + y + z == 2020]
+    print(lol[0])
+if __name__=='__main__':
+    with open('my_input.txt') as f:
+        numbers_in_file = list(sorted(map(int,f)))
+        _Q2(numbers_in_file)
