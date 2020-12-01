9ff794cb9c4a4710ff8f9c6da7d15b727072f1e5
SHANMUKH69 <37172194+SHANMUKH69@users.noreply.github.com>
2020-12-01 18:44:27 +0530

    finding 3 numbers and there product

--- Day1/Day1_q2_sol.py
+++ Day1/Day1_q2_sol.py
@@ -1,3 +1,4 @@
+from termcolor import colored
 import itertools
 '''
 https://docs.python.org/3/library/itertools.html#module-itertools
@@ -5,7 +6,8 @@ itertools.combinations(iterable, r): Return r length subsequences of elements fr
 '''
 def _Q2(n):
     lol = [x*y*z for x, y, z in itertools.combinations(map(int, n), 3) if x + y + z == 2020]
-    print(lol[0])
+    print(colored((lol[0]),'blue'))
+          
 if __name__=='__main__':
     with open('my_input.txt') as f:
         numbers_in_file = list(sorted(map(int,f)))
