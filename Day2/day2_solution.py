c86f51a45623cd9eb41231f349ab1486ad2d9035
SHANMUKH69 <37172194+SHANMUKH69@users.noreply.github.com>
2020-12-02 16:04:00 +0530

    day2 solution2

--- Day2/day2_solution.py
+++ Day2/day2_solution.py
@@ -4,14 +4,12 @@ def _Q2(x):
     In input argument there are three elements 1.atleast 2.atmost 3.password
     """
     return sum(atleast <= sum(password) <=atmost for atleast, atmost, password in x)
-
 if __name__=='__main__':
-    #using regular experssions remove : 
+    #using regular experssions remove 
     _listOf3 = [re.split('[: \-]', elements.strip()) for elements in open('day2_input.txt')]
     #printing the splited list
     #print(_listOf3)
     #assigining the _listOf3 to atleast, atmost, character, password
     #checking the characters(atmost, atleast) in existing password storing it in tuple
     x = [(int(atleast), int(atmost), [y == character for y in password]) for atleast, atmost, character, _, password in _listOf3]
-    #print(x)
     print(_Q2(x))
