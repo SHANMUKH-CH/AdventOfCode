ba1c40c3cda9a2c006db2d1a9576b2a1d176093c
SHANMUKH69 <37172194+SHANMUKH69@users.noreply.github.com>
2020-12-04 15:58:59 +0530

    changed password variable to passports

--- 2020/Day4/day4_Q1_sol.py
+++ 2020/Day4/day4_Q1_sol.py
@@ -3,7 +3,7 @@ def counting_valid_fields(x):
     list comp - tutorial
     https://github.com/SHANMUKH-CH/pythonPrograms/blob/main/tricks%20and%20tips/list%20comprehension.py
     '''
-    return(sum(1 if all(fields in passwords for fields in ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']) else 0 for passwords in x))
+    return(sum(1 if all(fields in passports for fields in ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']) else 0 for passports in x))
 
 if __name__=='__main__':
     _formated_input = [x.rstrip('\n') for x in open('day4_input.txt').read().split('\n\n')]
