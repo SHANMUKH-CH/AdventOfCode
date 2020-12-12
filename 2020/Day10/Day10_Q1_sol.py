2154b5efc7453b589cfe6ae49fa6891dfd6b0231
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2020-12-12 08:30:25 +0530

    part1

---
+++ 2020/Day10/Day10_Q1_sol.py
@@ -0,0 +1,13 @@
+def _Q1(x):
+    _one, _three = 0, 0
+    for x, y in zip(x, x[1:]):
+        if y-x == 1: _one +=1
+        elif y-x == 3: _three += 1
+    return (_one * _three)
+
+if __name__ == '__main__':
+    x = [int(l) for l in [l.rstrip('\n') for l in open('day10_input.txt','r')]]
+    x.append(0)
+    x.append(max(x)+3)
+    x.sort()
+    print(_Q1(x))
