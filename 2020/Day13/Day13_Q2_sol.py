b340192aba4d6c43d84141c76a2efe88ad732b6f
SHANMUKH69 <37172194+SHANMUKH69@users.noreply.github.com>
2020-12-13 12:26:24 +0530

    part 2

---
+++ 2020/Day13/Day13_Q2_sol.py
@@ -0,0 +1,13 @@
+from itertools import count
+from sympy.ntheory.modular import solve_congruence
+
+def _Q2(b):
+    return solve_congruence(*b)[0]
+
+if __name__ == '__main__':
+    with open('day13_input.txt','r') as x:
+        x = [x.strip() for x in x.readlines()]
+    _1st = int(x[0])
+    b_times = [(-i, int(x)) for i, x in enumerate(x[1].split(',')) if x != 'x']
+    #unpacking tuples check my python tricks and tips subdir in pythonprogram repo
+    print(_Q2(b_times))
